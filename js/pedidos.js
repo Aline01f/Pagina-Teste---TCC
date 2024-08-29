526759af30fd12bfc14d33e3d2cdff95fcba4397
Wellington731 <wellingtonpaulo731@gmail.com>
2024-08-29 16:34:55 -0300

    estilizando pedidos²

--- js/pedidos.js
+++ js/pedidos.js
@@ -2,7 +2,7 @@ function atualizarStatus(idPedido, status, aluno) {
     const elementoStatus = document.getElementById('status-' + idPedido);
     if (status === 'preparando') {
         elementoStatus.textContent = 'Status: Preparando';
-        elementoStatus.style.color = 'orange';
+        elementoStatus.style.color = '#E3AB00';
     } else if (status === 'pronto') {
         elementoStatus.textContent = 'Status: Pronto para Retirada';
         elementoStatus.style.color = 'green';
